b60ebe6e43e7ac3cdc709e77389fa5eae6d1eb7d
nikoo55 <1wesselsonik@hdsb.ca>
2025-02-26 14:25:01 -0500

    updateassignment1
    added variables

---
+++ assingment1niko.py
@@ -0,0 +1,32 @@
+# variable types
+
+import math
+string = "Blue"
+boolean = False
+integer = 3
+addition = 4 + 4
+subtraction = 5 - 3
+product = 5*2*9
+quotient_integer = 24/6
+quotient_float = 100/3
+square = math.sqrt(36)
+exponent = 6**9
+rem_1 = 24%6
+rem_2 = 24%5
+rem_3 = 24%7
+
+
+print("An example string is:", string)
+print("An example boolean:", boolean)
+print("An example integer:", integer)
+
+print("ADDITION OF 4+4 is:", addition) 
+print("SUBTRACTION OF 5-3 is:", subtraction)
+print("PRODUCT OF 5*2*9 is:", product)
+print("QUOTIENT INTEGER OF 24/6 is:", quotient_integer)
+print("QUOTIENT FLOAT OF 100/3 is:", quotient_float)
+print("SQUARE ROOT OF 36 is:", square)
+print("EXPONENT 6**9 is:", exponent)
+print("MODULUS is:", rem_1)
+print("MODULUS is:", rem_2)
+print("MODULUS is:", rem_3)
